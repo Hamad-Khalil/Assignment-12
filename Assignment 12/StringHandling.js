42df62bd1ffab20f37828d0a228d1f5b59222fef
Hamad Khalil <123974803+Hamad-Khalil@users.noreply.github.com>
2023-04-23 23:33:36 +0500

    Add files via upload

--- Assignment 12/StringHandling.js
+++ Assignment 12/StringHandling.js
@@ -23,6 +23,7 @@
 // 8. Write a js program to find total number of alphabets, digits or special character in a string.
 
 // 9. Write a js program to count total number of vowels and consonants in a string.
+// let string="A quick brown fox jumps over a lazy dog"
 
 // 10. Write a js program to count total number of words in a string.
 
@@ -39,11 +40,16 @@
 // console.log(occurenceOfE); // Output will be 1
 
 // 15. Write a js program to find last occurrence of a character in a given string.
-
+// let string="A quick brown fox jumps over a lazy dog"
+// // to find occurrence of l
+// console.log(string.lastIndexOf("l"));   // output will be 31
 // 16. Write a js program to search all occurrences of a character in given string.
 
 // 17. Write a js program to count occurrences of a character in given string.
-
+// let string="A quick brown fox jumps over a lazy dog"
+//  // to count occurence of "a"
+// let occurrenceOfChar= string.match(/a/g)
+// console.log(occurrenceOfChar.length);
 // 18. Write a js program to find highest frequency character in a string.
 
 // 19. Write a js program to find lowest frequency character in a string.
@@ -51,11 +57,19 @@
 // 20. Write a js program to count frequency of each character in a string.
 
 // 21. Write a js program to remove first occurrence of a character from string.
+// let string="Welcome to Pakistan" 
+// // to remove "e" from above string
+// let updatedStr= string.replace("e", "")
+// console.log(updatedStr);
 
 // 22. Write a js program to remove last occurrence of a character from string.
 
 // 23. Write a js program to remove all occurrences of a character from string.
 
+// let string="Welcome to Pakistan" 
+// // to remove all "e" from above string
+// let updatedStr= string.replaceAll("e", "")
+// console.log(updatedStr);
 // 24. Write a js program to remove all repeated characters from a given string.
 
 // 25. Write a js program to replace first occurrence of a character with another in a string.
@@ -63,20 +77,43 @@
 // 26. Write a js program to replace last occurrence of a character with another in a string.
 
 // 27. Write a js program to replace all occurrences of a character with another in a string.
+// let str="My name is Hamad Khalil" // to replace "a" with "e"
+// let newStr= str.replaceAll("a", "e")
+// console.log(newStr);
 
 // 28. Write a js program to find first occurrence of a word in a given string.
+// let str="This is a book. It is my book" // to find first occurance of "is"
+// let strInLowerCase= str.toLowerCase()
+// console.log(strInLowerCase.indexOf(" is "));
 
 // 29. Write a js program to find last occurrence of a word in a given string.
+// let str="This is a book. It is my book" 
+// let strInLowerCase= str.toLowerCase()// to find last occurance of "is"
+// console.log(strInLowerCase.lastIndexOf(" is "));
 
 // 30. Write a js program to search all occurrences of a word in given string.
 
 // 31. Write a js program to count occurrences of a word in a given string.
+// let str= "This is a book. It is my book"
+// let strInLowerCase= str.toLowerCase()
+// let newaStr= strInLowerCase.match(/ is /g)
+// console.log(newaStr.length);
+
+
 
 // 32. Write a js program to remove first occurrence of a word from string.
+// let str="This is a book. It is my book" // to find first occurance of "is"
 
 // 33. Write a js program to remove last occurrence of a word in given string.
+// let str="My father is a lawyer. He is a nice man" 
+// let newStr=str.
+
 
 // 34. Write a js program to remove all occurrence of a word in given string.
+// let str="My father is a lawyer. He is a nice man" // to remove is from given string
+// let newStr=str.replaceAll("is", "")
+// console.log(newStr);
+
 
 // 35. Write a js program to trim leading white space characters from given string.
 
@@ -85,3 +122,43 @@
 // 37. Write a js program to trim both leading and trailing white space characters from given string.
 
 // 38. Write a js program to remove all extra blank spaces from given string.
+
+
+
+
+
+
+
+
+
+
+
+
+
+let string= "Hello World"
+
+// console.log(string.includes("Hello"));
+// console.log(string.startsWith("Hello"));
+
+// let oInStr=(string.match(/o/g))
+// console.log(oInStr.length);
+
+// console.log(string.lastIndexOf('l'));
+// console.log(string.split(''));
+// console.log(string.charAt(1));
+// console.log(string.trim());
+// console.log(string.concat("", " of Information and technology"));
+// console.log(string.toLowerCase());
+// console.log(string.toUpperCase());
+// let newStr=string.replaceAll("l", "o")
+// console.log(newStr);
+// let newStr=string.replace("World", "Everyone")
+// console.log(newStr);
+// let newStr= string.substring(0,5)
+// console.log(newStr);
+// let newStr= string.slice(5, 11)
+// console.log(newStr)
+// console.log(string.length);
+
+
+
